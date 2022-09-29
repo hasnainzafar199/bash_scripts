b858b4d44e95bc10420abefede001d566ded7256
Hasnain Zafar <hasnainzafar99@gmail.com>
2022-09-28 21:43:31 -0400

    Creating a bash script to take a backup of directory.

---
+++ backup_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Author's Name: Hasnain Zafar
+# Date Created: 09/28/2022
+# Last Modified: 09/28/2022
+
+#Description: This script will take the backup of the directory where are running it in.
+#Usuage: Use backup_script.sh when you need a backup of a script.
+
+tar -cvzf backup_dir.tar .
+exit 0
