36f9b30a603ed89f2e5477d2de6ce927bc793e52
Hasnain Zafar <hasnainzafar99@gmail.com>
2022-10-03 11:40:39 -0400

    Make basic calculation for addition and substraction of upto 9 numbers

--- basic_calculator.sh
+++ basic_calculator.sh
@@ -4,4 +4,4 @@
 #Description: Adding and Substracting upto 9  numbers.
 #Usage: To add or subtract upto 9 numbers, first command line argument will be sign for addition or substraction.
 
-echo $(( ${2} ${1} ${3} ${1} ${4} ${1} ${5} ${1} ${6} ${1} ${7} ${1} ${8} ${1} ${9} ${1} ${10}))
+echo $(( ${2:-0} ${1} ${3:-0} ${1} ${4:-0} ${1} ${5:-0} ${1} ${6:-0} ${1} ${7:-0} ${1} ${8:-0} ${1} ${9:-0} ${1} ${10:-0}))
