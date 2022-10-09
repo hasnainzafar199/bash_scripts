e14634fcec398b9895f52f0358b89dd15ac46380
Hasnain Zafar <hasnainzafar99@gmail.com>
2022-10-09 10:42:03 -0400

    Moving scripts into folders based on their extensions.

--- folder_organiser.sh
+++ folder_organiser.sh
@@ -8,14 +8,14 @@ read -p "Enter the path of directory which contain files: " dir_path
 while read files;
 do
         case "$files" in
-                *.jpg|*.jpeg|*.png) mkdir -p images && mv *.jpg *.jpeg *.png images
-                *.mp3) mkdir -p audio && mv *.mp3 audio;;
-                *.doc|*.docx|*.txt|*pdf) mkdir -p documents && mv *.docx *.doc *.txt *pdf documents;;
-                *.xls|*.xlsx|*.csv) mkdir -p spreadsheets && mv *.xls *.xlsx *.csv spreadsheets;;
-#               *.sh) mkdir -p scripts && mv *.sh scripts;;
-                *.zip|*.tar|*.tar.gz|*.tar.bz2) mkdir -p archives && mv *.zip *.tar *.tar.gz *.tar.bz2 archives;;
-                *.ppt|*.pptx) mkdir -p presentations && mv *.ppt *.pptx presentations;;
-                *.mp4) mkdir -p video && mv *.mp4 video;;
+                *.jpg|*.jpeg|*.png) mkdir -p /images; mv "$files" /images;;
+                *.mp3) mkdir -p /audio; mv "$files" /audio;;
+                *.doc|*.docx|*.txt|*pdf) mkdir -p /documents; mv "$files" /documents;;
+                *.xls|*.xlsx|*.csv) mkdir -p /spreadsheets; mv "$files" /spreadsheets;;
+#               *.sh) mkdir -p /scripts; mv "$files" /scripts;;
+                *.zip|*.tar|*.tar.gz|*.tar.bz2) mkdir -p /archives; mv "$files" /archives;;
+                *.ppt|*.pptx) mkdir -p /presentations; mv "$files" /presentations;;
+                *.mp4) mkdir -p /video; mv "$files" /video;;
                 *);;
         esac
 done < <(ls $dir_path)
