1b550b3c2665e4e2ce7850468ded5df9c2d082a6
Hasnain Zafar <hasnainzafar199@gmail.com>
2022-10-29 15:13:51 -0400

    Create copypublickeys.sh
    Shell script to copy ssh public keys on remote machines.

---
+++ copypublickeys.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#To copy as many public keys for ssh login into remote machines.
+#Create a file which contains IP addresses of each remote machine on each line.
+for each_ip in $(cat fileofips.txt);
+do
+        ssh $each_ip mkdir -m 700 ~/.ssh
+        scp -pr ~/.ssh/id_rsa.pub root@$each_ip:~/.ssh/authorized_keys;
+done
