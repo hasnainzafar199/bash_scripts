ad9e55c7f8692993e94d2ca89314286f267e6259
Hasnain Zafar <hasnainzafar199@gmail.com>
2022-11-15 13:56:35 -0500

    Update copypublickeys.sh

--- copypublickeys.sh
+++ copypublickeys.sh
@@ -4,6 +4,6 @@
 #Create a file which contains IP addresses of each remote machine on each line.
 for each_ip in $(cat fileofips.txt);
 do
-        ssh $each_ip mkdir -m 700 ~/.ssh
+        ssh $each_ip mkdir -m 700 ~/.ssh 2>/dev/null
         scp -pr ~/.ssh/id_rsa.pub root@$each_ip:~/.ssh/authorized_keys;
 done
